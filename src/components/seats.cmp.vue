026854de635d3e9683fbea52da138361a7d93a38
Roy Margalit <roy4499@gmail.com>
2020-12-10 14:22:15 +0200

    first 3

---
+++ src/components/seats.cmp.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="seats">
+    <h1>Elad and Roy's dream theatre</h1>
+    <ul class="seat-container" v-for="(i, idx) in rowNum" :key="idx">
+      {{
+        i
+      }}
+      <ul class="col-container" v-for="(j, idx) in colNum" :key="idx">
+        <li :class="[`seat seat-${i}-${j}`]" :ref="[`mySeat-${i}-${j}`]" @click="clickSeat(i, j)">
+          {{ j }}
+        </li>
+      </ul>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      rowNum: 8,
+      colNum: 8,
+    };
+  },
+  methods: {
+    clickSeat(i, j) {
+      console.log(this.$refs,this,i, j);
+      // var cell= `mySeat-${i}-${j}`
+      // console.log(this.$refs.mySeat);
+
+    },
+  },
+  mounted(){
+    this.$refs.mySeat
+    // console.log(elSeat)
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.seat-container {
+  display: flex;
+  justify-content: center;
+  list-style-type: none;
+  padding: 0;
+  .seat {
+    display: inline-block;
+    margin: 0 5px;
+    padding: 10px;
+    border-radius: 50%;
+    background-color: lightblue;
+    cursor: pointer;
+  }
+  .seat-2-3 {
+    background-color: lightcoral;
+  }
+  .clicked {
+    background-color: lightseagreen;
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <seats />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import seats from '@/components/seats.cmp.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    seats
   }
 }
 </script>
